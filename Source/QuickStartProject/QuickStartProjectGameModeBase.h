8a65a0ff4d411e60181b908802cf79cd803d8b95
dmdylan <51095343+dmdylan@users.noreply.github.com>
2020-05-13 22:22:03 -0400

    Add files via upload
    testing unreal engine

---
+++ Source/QuickStartProject/QuickStartProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuickStartProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUICKSTARTPROJECT_API AQuickStartProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
